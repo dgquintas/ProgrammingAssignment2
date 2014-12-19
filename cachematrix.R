0909903b4e3e7c0c5fbc174262e5c403ad18abd2
dgq <dgq@localhost>
2014-12-19 07:45:10 +0100

    Completed assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions implement:
+## - makeCacheMatrix: a class-like data structure storing a matrix 
+##                    and its inverse.
+## - cacheSolve: a function that, given an "instance" of a "cache 
+##               matrix", returns its inverse, avoiding its computation
+##               if it's already cached.
 
-## Write a short comment describing this function
-
+## Create a class-like instance storing a matrix and its associated
+## inverse.
+##
+## Note that it's the user's responsibility to make sure the stored
+## matrix and its inverse are in sync. No calculations/check are
+## performed in the exposed functions.
+##
+## It exposes the following functions:
+## - set(new_matrix): Sets the given matrix as the stored matrix.
+## - get(): Returns the stored matrix.
+## - setinverse(new_inverse): Set new_inverse to be the new inverse
+##                            for the stored matrix.
+## - getinverse(): Returns the stored inverse.
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
+    set <- function(new_matrix) {
+        x <<- new_matrix
+        # New matrix stored: invalidate cached inverse (if any).
+        cached_value <<- NULL
+    }
+    get <- function() x
 
+    setinverse <- function(new_inverse) inverse <<- new_inverse
+    getinverse <- function() inverse
+
+    list(set = set, get = get,
+         setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return the inverse of the input cache matrix 'x'.
+## If a cached value for the inverse is found in 'x', it'll
+## be returned. Otherwise, the inverse will be calculated and
+## 'x' updated with its value.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if (is.null(inverse)) {
+        # The inverse wasn't cached: calculate it and store it in 
+        # the cache.
+        inverse <- solve(x$get(), ...) 
+        x$setinverse(inverse)
+    } else {
+        # Quick and dirty way of checking whether we are using the
+        # cached inverse.
+        message("Cache hit!")
+    }
+    inverse
 }
